fdeaaa8808530b1666fc0b66f88467b2b21b2410
Jimenarosello <43755493+Jimenarosello@users.noreply.github.com>
2018-10-05 09:38:55 -0300

    Update TrainingApplication.java

--- src/main/java/Wolox/training/TrainingApplication.java
+++ src/main/java/Wolox/training/TrainingApplication.java
@@ -2,8 +2,6 @@ package Wolox.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.*;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class TrainingApplication {
