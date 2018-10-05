d7ffa7fffdb7ba975a58758746d8dae79e6e5fe0
Jimenarosello <43755493+Jimenarosello@users.noreply.github.com>
2018-10-05 09:31:20 -0300

    Update TrainingApplication.java

--- src/main/java/Wolox/training/TrainingApplication.java
+++ src/main/java/Wolox/training/TrainingApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.*;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories("org.baeldung.persistence.repo")
-@EntityScan("org.baeldung.persistence.model")
 @SpringBootApplication
 public class TrainingApplication {
 
